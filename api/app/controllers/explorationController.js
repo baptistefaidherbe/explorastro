c3bb9c59656a5af8980ce5f03155b44ebefba6d5
baptiste <bapt78@gmail.com>
2021-09-07 00:35:38 +0200

    add method getExplorationById in controller + route /exploration/:id

--- api/app/controllers/explorationController.js
+++ api/app/controllers/explorationController.js
@@ -6,10 +6,23 @@ const explorationController = {
       if (error) {
         console.trace(error);
       } else {
-        res.json({ exploration : response.rows });
+        res.json({ explorations : response.rows });
       }
     });
   },
+  getExplorationById: (req, res) => {
+    const promo_id = Number(req.params.id);
+    dataMapper.getExplorationByIdRequest(
+        promo_id, 
+        (error, response) => {
+            if (error) {
+                console.trace(error);
+            } else {
+                res.json({ exploration : response.rows[0] })
+            }
+        }
+    );
+}
 };
 
 module.exports = explorationController;
