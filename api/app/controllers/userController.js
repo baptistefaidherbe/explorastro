3894a14347a841d747ca3ad6cc92335bd6a509eb
baptiste <bapt78@gmail.com>
2021-09-07 11:45:28 +0200

    add method getUsers in controllerUser

--- api/app/controllers/userController.js
+++ api/app/controllers/userController.js
@@ -1,8 +1,9 @@
-const dataMapper = require("../dataMapper/dataExploration.js");
+const {dataUser} = require("../dataMapper/");
+
 
 const userController = {
     getUsers: (req, res) => {
-      dataMapper.getUsersRequest((error, response) => {
+        dataUser.getUsersRequest((error, response) => {
         if (error) {
           console.trace(error);
         } else {
@@ -10,47 +11,7 @@ const userController = {
         }
       });
     },
-    getExplorationById: (req, res) => {
-      const promo_id = Number(req.params.id);
-      dataMapper.getExplorationByIdRequest(promo_id, (error, response) => {
-        if (error) {
-          console.trace(error);
-        } else {
-          res.json({ exploration: response.rows[0] });
-        }
-      });
-    },
-    deleteExploration: (req, res) => {
-      const id = Number(req.params.id);
-      dataMapper.deleteExplorationRequest(id, (error, response) => {
-        if (error) {
-          console.trace(error);
-        } else {
-          res.json({ exploration: response.rows[0] });
-        }
-      });
-    },
-    createExploration: (req, res) => {
-      const { name, author_id } = req.body;
-      dataMapper.createExplorationRequest(name, author_id, (error, response) => {
-        if (error) {
-          console.trace(error);
-        } else {
-          res.json({ exploration: response.rows[0] });
-        }
-      });
-    },
-    updateExploration: (req, res) => {
-      const id = Number(req.params.id);
-      const {name, description, geog, date, max_participants, is_published} = req.body;
-      dataMapper.updateExplorationRequest(id, name, description, geog, date, max_participants, is_published,(error, response) => {
-        if (error) {
-          console.trace(error);
-        } else {
-          res.json({ exploration: response.rows[0] });
-        }
-      });
-    },
+   
   };
   
   module.exports = userController;
--- api/app/dataMapper/dataUser.js
+++ api/app/dataMapper/dataUser.js
@@ -1,7 +1,12 @@
 const client = require("../database");
 
 const dataUser = {
- 
-  
+    getUsersRequest: (callback) => {
+    const explorations_query = {
+      text: `SELECT * FROM "user";`,
+    };
+
+    client.query(explorations_query, callback);
+  },
 };
 module.exports = dataUser;
