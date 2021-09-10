02a2303a34f2d5c343e72790fed9061aa1806598
baptiste <bapt78@gmail.com>
2021-09-10 17:29:32 +0200

    move code nodemailer to folder utils

--- api/app/controllers/forgotPaswordController.js
+++ api/app/controllers/forgotPaswordController.js
@@ -1,39 +1,13 @@
 const jwt = require("../utils/token");
 const bcrypt = require("bcrypt");
-const hbs = require("nodemailer-express-handlebars");
-const EMAIL_MAILER = process.env.MAILER_EMAIL_ID;
-const PASSWORD_MAILER = process.env.MAILER_PASSWORD;
-const BASE_URL = process.env.BASE_URL;
-const nodemailer = require("nodemailer");
+const mailer = require("../utils/mailer");
 const { dataAuth } = require("../dataMapper/");
-const path = require("path");
 const MESSAGE = require("../constant/message");
-
 
 const forgotPasswordController = {
   forgotPassword: async (req, res) => {
     try {
       const { email } = req.body;
-
-      const smtpTransport = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: EMAIL_MAILER,
-          pass: PASSWORD_MAILER,
-        },
-      });
-
-      var handlebarsOptions = {
-        viewEngine: {
-          extName: ".handlebars",
-          partialsDir: path.resolve(__dirname, "../templates"),
-          defaultLayout: false,
-        },
-        viewPath: path.resolve(__dirname, "../templates"),
-        extName: ".html",
-      };
-
-      smtpTransport.use("compile", hbs(handlebarsOptions));
 
       dataAuth.findUserMailRequest(email, (error, response) => {
         if (error) {
@@ -42,22 +16,7 @@ const forgotPasswordController = {
           if (response.rows.length !== 0) {
             const { id, email } = response.rows[0];
             const token = jwt.generateToken(id, email, "10m");
-            var data = {
-              to: email,
-              from: EMAIL_MAILER,
-              template: "forgot-password-email",
-              subject: "Réinitialiser mon mot de passe",
-              context: {
-                url: `${BASE_URL}/auth/reset_password?token=` + token,
-              },
-            };
-            smtpTransport.sendMail(data, function (err, info) {
-              if (err) {
-                console.log(err);
-              } else {
-                return res.json(MESSAGE.SUCCESS_SEND_MAIL);
-              }
-            });
+            mailer.sendMail(email, token);
             dataAuth.updateTokenTmpRequest(id, token, (error, response) => {
               if (error) {
                 console.trace(error);
@@ -94,7 +53,6 @@ const forgotPasswordController = {
                 const { id } = response.rows[0];
 
                 const passwordHash = bcrypt.hashSync(password, 10);
-                console.log(passwordHash);
                 dataAuth.updatePasswordRequest(
                   id,
                   passwordHash,
